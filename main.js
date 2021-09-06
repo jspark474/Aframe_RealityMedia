5ebd0dcab108d49ae970e170ed51e8216b8ad9ac
Glitch (ar-starter-kit) <none>
2021-09-06 12:03:45 +0000

    👻🐷 Checkpoint ./main.js:300780/4202

--- main.js
+++ main.js
@@ -1,63 +1,72 @@
 /* jshint esversion: 9 */
 /* global THREE, AFRAME */
-(function () {
-	"use strict";
-  
-  AFRAME.registerComponent('hide-on-hit-test-start', {
-    init: function () {
+(function() {
+  "use strict";
+
+  AFRAME.registerComponent("hide-on-hit-test-start", {
+    init: function() {
       var self = this;
-      this.el.sceneEl.addEventListener('ar-hit-test-start', function () {
+      this.el.sceneEl.addEventListener("ar-hit-test-start", function() {
         self.el.object3D.visible = false;
       });
-      this.el.sceneEl.addEventListener('exit-vr', function () {
+      this.el.sceneEl.addEventListener("exit-vr", function() {
         self.el.object3D.visible = true;
       });
     }
   });
+})();
 
-}());
-
-window.addEventListener('DOMContentLoaded', function () {
-
-	const sceneEl = document.querySelector('a-scene');
-	const message = document.getElementById('dom-overlay-message');
+window.addEventListener("DOMContentLoaded", function() {
+  const sceneEl = document.querySelector("a-scene");
+  const message = document.getElementById("dom-overlay-message");
 
   // If the user taps on any buttons or interactive elements we may add then prevent
   // Any WebXR select events from firing
-  message.addEventListener('beforexrselect', e => {
+  message.addEventListener("beforexrselect", e => {
     e.preventDefault();
   });
 
-  // Hit testing is available
-  this.addEventListener('selectstart', function (e) {
-    console.log(e);
-  }, { once: true });
-  
-	sceneEl.addEventListener('enter-vr', function () {
-		if (this.is('ar-mode')) {
-      
+  sceneEl.addEventListener("enter-vr", function() {
+    if (this.is("ar-mode")) {
       // Entered AR
-			message.textContent = '';
+      message.textContent = "";
+
+      sceneEl.xrSession.addEventListener("selectstart", function(e) {
+        const inputSource = e.inputSource;
+        e.preventDefault();
+      });
 
       // Hit testing is available
-			this.addEventListener('ar-hit-test-start', function () {
-				message.innerHTML = `Scanning environment, finding surface.`;
-			}, { once: true });
+      this.addEventListener(
+        "ar-hit-test-start",
+        function() {
+          message.innerHTML = `Scanning environment, finding surface.`;
+        },
+        { once: true }
+      );
 
       // Has managed to start doing hit testing
-			this.addEventListener('ar-hit-test-achieved', function () {
-				message.innerHTML = `Select the location to place<br />By tapping on the screen or selecting with your controller.`;
-			}, { once: true });
+      this.addEventListener(
+        "ar-hit-test-achieved",
+        function() {
+          message.innerHTML = `Select the location to place<br />By tapping on the screen or selecting with your controller.`;
+        },
+        { once: true }
+      );
 
       // User has placed an object
-			this.addEventListener('ar-hit-test-select', function () {
-        // Object placed for the first time
-		    message.textContent = 'Well done!';
-      } , {once: true});
-		}
-	});
+      this.addEventListener(
+        "ar-hit-test-select",
+        function() {
+          // Object placed for the first time
+          message.textContent = "Well done!";
+        },
+        { once: true }
+      );
+    }
+  });
 
-	sceneEl.addEventListener('exit-vr', function () {
-		message.textContent = 'Exited Immersive Mode';
-	});
-})
+  sceneEl.addEventListener("exit-vr", function() {
+    message.textContent = "Exited Immersive Mode";
+  });
+});
