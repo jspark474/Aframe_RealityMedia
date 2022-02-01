cf12b07730e4ccc1322101eddb170cd466e1bf99
Glitch (ar-starter-kit) <none>
2022-02-01 15:54:23 +0000

    💽📌 Checkpoint ./lightmap.js:300780/165

--- lightmap.js
+++ lightmap.js
@@ -1,15 +1,16 @@
-/* global AFRAME */
+/* global AFRAME, THREE */
 AFRAME.registerComponent('lightmap', {
   schema: {
     type: "map"
   },
   init() {
     this.el.addEventListener('object3dset', this.update.bind(this));
+	  this.texture = new THREE.TextureLoader().load( typeof this.data === 'string' ? this.data : this.data.src );
   },
   update() {
     this.el.object3D.traverse(function (o) {
       if (o.material) {
-        o.material.lightMap = this.data;
+        o.material.lightMap = this.texture;
       }
     }.bind(this));
   }
