9c7850526df365ed8f5fb202142127c1e5eb2840
Glitch (ar-starter-kit) <none>
2022-02-02 12:11:14 +0000

    👷🚇 Checkpoint ./model-utils.js:300780/2706 ./index.html:300780/13

--- model-utils.js
+++ model-utils.js
@@ -11,31 +11,50 @@ AFRAME.registerComponent('lightmap', {
     },
     filter: {
       default: ''
+    },
+    basis: {
+      default: false
     }
   },
   init() {
     
+    const src = typeof this.data.src === 'string' ? this.data.src : this.data.src.src;
+    
     const ktx2Loader = new KTX2Loader();
     ktx2Loader.setTranscoderPath( 'https://cdn.jsdelivr.net/npm/three@0.136.0/examples/js/libs/basis/' );
     ktx2Loader.detectSupport( this.el.sceneEl.renderer );
-    loader.setDRACOLoader(draco);
-    loader.setKTX2Loader (ktx2Loader);
+    this.texturePromise = new Promise (function (resolve, reject) {
+      
+      if (this.data.basis) {
+        ktx2Loader.load( src, function ( texture ) {
+          texture.flipY = false;
+          console.log(texture);
+          resolve(texture);
+        }, function () {
+          console.log( 'Loading Basis: ' + src );
+        }, function (e) {
+          reject(e);
+        });
+      } else {
+        const texture = new THREE.TextureLoader().load( typeof this.data.src === 'string' ? this.data.src : this.data.src.src );
+        texture.flipY = false;
+        resolve(texture);
+      }
+    }.bind(this));
 
     this.el.addEventListener('object3dset', this.update.bind(this));
-	  this.texture = new THREE.TextureLoader().load( typeof this.data.src === 'string' ? this.data.src : this.data.src.src );
-    this.texture.flipY = false;
     this.materials = new Map();
   },
   update() {
     const filters = this.data.filter.trim().split(',');
-    this.el.object3D.traverse(function (o) {
+    this.el.object3D.traverse(async function (o) {
       if (o.material) {
         if (filters.some(filter => o.material.name.includes(filter))) {
           const sceneEl = this.el.sceneEl;
           const m = o.material;
           o.material = this.materials.has(m) ? this.materials.get(m) : new THREE.MeshPhongMaterial({
             name: 'phong_' + m.name,
-            lightMap: this.texture,
+            lightMap: await this.texturePromise,
             lightMapIntensity: this.data.intensity,
             color: m.color,
             map: m.map,
