9a13ac4b4567c38f1ca0b81088a46765a1768da7
Glitch (aframe-xr-starterkit) <none>
2022-02-25 17:51:08 +0000

    🐺🐔 Checkpoint ./pose-recorder.js:300780/817

--- pose-recorder.js
+++ pose-recorder.js
@@ -1,4 +1,19 @@
-async function recordPose(frames=180, inputs) {
-  const {} = await import('https://cdn.jsdelivr.net/npm/handy-work/build/handy-controls.min.js');
-  
+const handyWorkModule = import('https://cdn.jsdelivr.net/npm/handy-work/build/handy-work.min.js');
+async function recordPose(frames=180, inputSources, referenceSpace) {
+  const {generatePose} = await handyWorkModule;
+  const tempHands = {};
+	for (const source of inputSources) {
+		if (!source.hand) continue;
+		tempHands[source.handedness] = source.hand;
+	}
+	if (tempHands.left && tempHands.right) {
+		const size = tempHands.left.size;
+		const outData = new Float32Array((
+			1 +         // store size
+			size * 16 + // left hand
+			size * 16 + // right hand
+			size +      // weighting for individual joints left hand
+			size        // weighting for individual joints right hand
+		) * size);
+  }
 }
