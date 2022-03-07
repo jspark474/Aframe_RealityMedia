e20cf70bd8743288e2017f5bb2b86c2355fa1fb0
Glitch (aframe-xr-starterkit) <none>
2022-03-07 17:44:46 +0000

    🌿👯 Checkpoint ./main.js:300780/140

--- main.js
+++ main.js
@@ -26,20 +26,20 @@ AFRAME.registerComponent("origin-on-ar-start", {
   }
 });
 
-// AFRAME.registerComponent("xr-follow", {
-//   schema: {},
-//   init() {
-//   },
-//   tick() {
-//     const scene = this.el.sceneEl;
-//     const cameraObject = scene.camera;
-//     const camera = (scene.is('ar-mode') || scene.is('vr-mode')) ?
-//           scene.renderer.xr.getCamera(cameraObject) : cameraObject;
-//     const object3D = this.el.object3D;
-//     camera.getWorldPosition(object3D.position);
-//     this.el.parentNode.object3D.worldToLocal(object3D.position);
-//   }
-// });
+AFRAME.registerComponent("xr-follow", {
+  schema: {},
+  init() {
+  },
+  tick() {
+    const scene = this.el.sceneEl;
+    const cameraObject = scene.camera;
+    const camera = (scene.is('ar-mode') || scene.is('vr-mode')) ?
+          scene.renderer.xr.getCamera(cameraObject) : cameraObject;
+    const object3D = this.el.object3D;
+    camera.getWorldPosition(object3D.position);
+    object3D.parent.worldToLocal(object3D.position);
+  }
+});
 
 AFRAME.registerComponent("exit-on", {
   schema: {
