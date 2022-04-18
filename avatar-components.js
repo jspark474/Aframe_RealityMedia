bdb44d47bf3875b330e35f1a50d52f470ea7c633
Glitch (aframe-xr-starterkit) <none>
2022-04-18 15:05:12 +0000

    👕🎇 Checkpoint ./.glitch-assets:300780/786 ./index.html:300780/190 ./avatar-components.js:300780/7423 ./main.js:300780/659

--- avatar-components.js
+++ avatar-components.js
@@ -1,39 +1,18 @@
 /* jshint esversion: 9 */
 /* global THREE, AFRAME, Ammo */
 
-AFRAME.registerComponent("match-position-by-id", {
-  schema: {
-    default: ''
-  },
-  tick() {
-    let obj;
-    
-    if (this.data === 'xr-camera') {
-      const xrCamera = this.el.sceneEl.renderer.xr.getCameraPose();
-      if (xrCamera) {
-        this.el.object3D.position.copy(xrCamera.transform.position);
-        this.el.object3D.quaternion.copy(xrCamera.transform.orientation);
-        return;
-      }
-      obj = this.el.sceneEl.camera;
-    } else {
-      obj = document.getElementById(this.data).object3D;
-    }
-    if (obj) {
-      this.el.object3D.position.copy(obj.position);
-      this.el.object3D.quaternion.copy(obj.quaternion);
-    }
-
-  }
-});
-
-bodyBits:
+avatarStuff:
 {
   const tempVector3 = new THREE.Vector3();
   const tempQuaternionA = new THREE.Quaternion();
   const tempQuaternionB = new THREE.Quaternion();
   const zAxis = new THREE.Vector3(0,0,1);
   const yAxis = new THREE.Vector3(0,1,0);
+  const tempVectorShoulderPos = new THREE.Vector3();
+  const tempVectorHandPos = new THREE.Vector3();
+  const normal = new THREE.Vector3();
+  const c0 = new THREE.Vector3();
+
   AFRAME.registerComponent("torso", {
     schema: {
       head: {
@@ -76,7 +55,7 @@ bodyBits:
           this.offset = new THREE.Vector3();
           this.offset.subVectors(this.head.position, this.torso.position);
         }
-        
+
         this.head.getWorldPosition(this.torso.position);
         this.torso.position.sub(this.offset);
         this.torso.parent.worldToLocal(this.torso.position);
@@ -88,10 +67,11 @@ bodyBits:
     }
   });
   
-  const tempVectorShoulderPos = new THREE.Vector3();
-  const tempVectorHandPos = new THREE.Vector3();
-  const normal = new THREE.Vector3();
-  const c0 = new THREE.Vector3();
+  function pointBoneAt(boneToPoint, globalPosition) {
+    
+  }
+  
+  
   AFRAME.registerComponent("elbow", {
     multiple: true,
     schema: {
@@ -148,53 +128,49 @@ bodyBits:
           if (this.elbow) this.elbow = this.elbow.object3D;
         }
       }
-      
-      
+
+
       if (this.shoulder && this.hand && this.elbow) {
-        const $o = this.elbow;
-        
         // add a little gravity
-        $o.position.addScaledVector(this.data.bias, 9.8 * delta/1000);
-        
+        this.elbow.position.addScaledVector(this.data.bias, 9.8 * delta/1000);
+
         // Local hand position
         this.hand.getWorldPosition(tempVectorHandPos);
-        $o.parent.worldToLocal(tempVectorHandPos);
-        
+        this.elbow.parent.worldToLocal(tempVectorHandPos);
+
         // Local Shoulder position
         this.shoulder.getWorldPosition(tempVectorShoulderPos);
-        $o.parent.worldToLocal(tempVectorShoulderPos);
-        
+        this.elbow.parent.worldToLocal(tempVectorShoulderPos);
+
         const r1 = this.data.upperArmLength;
         const r2 = this.data.foreArmLength;
-        
+
         const d=tempVector3.subVectors(tempVectorShoulderPos,tempVectorHandPos).length();
-        
+
         // if arm is stretched longer than bones then elbow is placed proportionally
         if (d >= r1 + r2) {
-          $o.position.lerpVectors(tempVectorShoulderPos,tempVectorHandPos,r1/(r1+r2));
-          tempVector3.subVectors($o.position, tempVectorShoulderPos).normalize();
-          this.shoulder.quaternion.setFromUnitVectors(yAxis, tempVector3);
-          tempVector3.subVectors(tempVectorHandPos, $o.position).normalize();
-          $o.quaternion.setFromUnitVectors(yAxis, tempVector3);
+          this.elbow.position.lerpVectors(tempVectorShoulderPos,tempVectorHandPos,r1/(r1+r2));
+          this.shoulder.quaternion.setFromUnitVectors(yAxis, tempVector3.subVectors(this.elbow.position, tempVectorShoulderPos).normalize());
+          this.elbow.quaternion.setFromUnitVectors(yAxis, tempVector3.subVectors(tempVectorHandPos, this.elbow.position).normalize());
           return
         }
-        
+
         // One bone sphere is inside the other, i.e. hand is placed on shoulder
         // and one bone is smaller than the other and do not intersect
         if (Math.max(r1,r2) >= (d + Math.min(r1,r2))) {
           // this weird so do nothing
           return
         }
-        
+
         // The usual situation the two spheres intersesct so find the circle at the intersection point.
         const d1 = 0.5*(d+(r1*r1-r2*r2)/d);
         normal.subVectors(tempVectorHandPos, tempVectorShoulderPos).normalize();
         const r = Math.sqrt(r1*r1-d1*d1);
-        
+
         // The intersection of the spheres form a circle radius r
         // with center c0
         c0.copy(tempVectorShoulderPos).addScaledVector(normal, d1);
-        
+
         // We have a plane with normal that contains c0
         // We want to place the object where a vector n from the objects original position (p0) intersects the plane
         // n dot p = c0.n
@@ -202,16 +178,14 @@ bodyBits:
         // t n.n + p0.n = c0.n
         // t = n.p0 - n.c0 / (n.n)
         // p[new] = p0 + t n
-        
-        const t = normal.dot(tempVector3.copy(c0).sub($o.position));
-        
-        // move elbow inline with elbow plane and place it on the circle
-        $o.position.addScaledVector(normal, t).sub(c0).setLength(r).add(c0);
 
-        tempVector3.subVectors($o.position, tempVectorShoulderPos).normalize();
-        this.shoulder.quaternion.setFromUnitVectors(yAxis, tempVector3);
-        tempVector3.subVectors(tempVectorHandPos, $o.position).normalize();
-        $o.quaternion.setFromUnitVectors(yAxis, tempVector3);
+        const t = normal.dot(tempVector3.copy(c0).sub(this.elbow.position));
+
+        // move elbow inline with elbow plane and place it on the circle
+        this.elbow.position.addScaledVector(normal, t).sub(c0).setLength(r).add(c0);
+
+        this.shoulder.quaternion.setFromUnitVectors(yAxis, tempVector3.subVectors(this.elbow.position, tempVectorShoulderPos).normalize());
+        this.elbow.quaternion.setFromUnitVectors(yAxis, tempVector3.subVectors(tempVectorHandPos, this.elbow.position).normalize());
       }
     }
   });
@@ -242,12 +216,12 @@ bodyBits:
       const o = this.el.object3D;
       pp.getWorldQuaternion(tempQuaternionA).invert();
       o.getWorldQuaternion(p.quaternion).premultiply(tempQuaternionA);
-      
+
       if (this.data.yAxis) {
         tempVector3.set(0,1,0).applyQuaternion(o.quaternion);
         o.quaternion.setFromUnitVectors(tempVector3, zAxis);
       }
-      
+
       o.getWorldPosition(p.position);
       pp.worldToLocal(p.position);
       this.part.scale.copy(o.scale);
